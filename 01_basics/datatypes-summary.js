1b3d5d7f9370435d0710e6b63b9bfcf94c80a51b
Amitkumar-2001 <128308667+Amitkumar-2001@users.noreply.github.com>
2025-01-20 11:52:40 +0000

    math and nums

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -35,3 +35,23 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// +++++++++++++++++++++++++++++++
+// stack(primitive), Heap(Non-primitive)
+let
+myYoutubename="Amitkumardasdotcom"
+let anothername=myYoutubename
+anothername="chaiaurcode"
+console.log(anothername);
+console.log(myYoutubename);
+
+let userOne ={
+    email: "user@google.com",
+    upi:"user@ybl"
+}
+
+let userTwo= userOne
+userTwo.email="Amit@google.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
